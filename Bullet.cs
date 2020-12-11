b31f3e70dd8692dc7bdb62e038d2d4e4fddbb44a
Everardus Tol <43673344+jobtolmop@users.noreply.github.com>
2020-12-11 11:20:09 +0100

    Added Scripts

---
+++ Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private int speed = 10;
+
+    private void Update()
+    {
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+
+}
---
+++ PlayerShooting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private int prefabsToSpawn = 100;
+    [SerializeField] private float reloadTime = 0.3f;
+
+    private List<GameObject> bullets = new List<GameObject>();
+
+    private int currIndex;
+
+    private float timer;
+
+    private void Start()
+    {
+        for (int i = 0; i < prefabsToSpawn; i++)
+        {           
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullets.Add(bullet);
+        }
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space)&& timer >= reloadTime)
+        {
+            timer = 0;
+            GameObject bullet = bullets[currIndex];
+            bullet.transform.position = transform.position;
+            bullet.SetActive(true);
+            currIndex++;
+
+            if (currIndex >= bullets.Count - 1)
+            {
+                currIndex = 0;
+            }
+        }
+    }    
+}
